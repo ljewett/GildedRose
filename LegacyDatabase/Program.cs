41a4cf387f5bab2835949ef6e35b422a77eb7bf3
Logan Jewett <ljewett@pillartechnology.com>
2018-02-11 17:59:19 -0600

    reverting solution

---
+++ LegacyDatabase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Npgsql;
+using System.Data;
+
+namespace LegacyDatabase
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connString = "Host=localhost;Username=postgres;Password=example;Database=legacy;Port=5432";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT * FROM Items", conn);                  
+                DataSet items= new DataSet();
+                adapter.Fill(items);
+                foreach (DataRow row in items.Tables[0].Rows)
+                {
+                    Console.WriteLine(row[0]);
+                }
+            }
+        }
+    }
+}
